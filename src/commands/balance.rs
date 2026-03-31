ffe87127dc0337c076e7d7c6a96b1d2558c4bddf
kolpa01 <kolpabili@gmail.com>
2026-03-31 17:43:53 +0200

    Improved /balance command

--- src/commands/balance.rs
+++ src/commands/balance.rs
@@ -1,10 +1,18 @@
-use serenity::all::{CommandInteraction, Context, CreateCommand, CreateInteractionResponse, CreateInteractionResponseMessage};
+use serenity::all::{CommandInteraction, Context, CreateCommand, CreateCommandOption, CreateEmbed, CreateEmbedAuthor, CreateEmbedFooter, CreateInteractionResponse, CreateInteractionResponseMessage};
 
 use crate::{DbPool, morex};
 
 pub fn register() -> CreateCommand {
     CreateCommand::new("balance")
-        .description("Bal")
+        .description("View balance.")
+        .description_localized("pl", "Wyświetl stan konta.")
+        .add_option(
+            CreateCommandOption::new(serenity::all::CommandOptionType::User, "member", "Select a member.")
+            .name_localized("pl", "osoba")
+            .description_localized("pl", "Wybierz osobę")
+            .required(false)
+        )
+
 }
 
 pub async fn run(ctx: &Context, interaction: &CommandInteraction) {
@@ -14,7 +22,28 @@ pub async fn run(ctx: &Context, interaction: &CommandInteraction) {
     morex::account::create_account(&interaction.user.id, &pool).await;
     let balance = morex::economy::get_bank_account(&interaction.user.id, &pool).await;
 
-    let response = CreateInteractionResponseMessage::new().content(format!("Your wallet: {}\nYour bank: {}", balance.wallet, balance.bank));
+    let av = interaction.user.avatar_url().unwrap_or_else(|| interaction.user.default_avatar_url());
+    
+
+    let embed = CreateEmbed::new()
+        .color((b'\xce', b'\x4d', b'\xd5'))
+        .author(
+            CreateEmbedAuthor::new(format!("Balance {}", &interaction.user.name))
+            .icon_url(av)
+        )
+        .fields(vec![
+                ("Coins in the wallet:", format!("{} COIN", balance.wallet), false),
+                ("Coins in the bank:", format!("{} COIN", balance.bank), false),
+                ("All coins:", format!("{} COIN", balance.bank + balance.wallet), false),
+            ]
+        )
+        .footer(
+            CreateEmbedFooter::new("Version 0.1.0")
+        );
+        
+    let response = CreateInteractionResponseMessage::new()
+        .embed(embed);
+        // .content(format!("Your wallet: {}\nYour bank: {}", balance.wallet, balance.bank));
     let response = CreateInteractionResponse::Message(response);
 
     let msg = interaction.create_response(&ctx.http, response).await;
