da12ceb933be82996836666513d18f6547fc17a7
kolpa01 <kolpabili@gmail.com>
2026-03-31 19:57:14 +0200

    Handle the possible exception when creating the table

--- src/main.rs
+++ src/main.rs
@@ -56,7 +56,8 @@ async fn main() {
 
     sqlx::query!("CREATE TABLE IF NOT EXISTS users (user_id bigint PRIMARY KEY, wallet bigint NOT NULL, bank bigint NOT NULL, hp bigint NOT NULL, level bigint NOT NULL, xp bigint NOT NULL, total_xp bigint NOT NULL, version varchar(8) NOT NULL, timestamp timestamptz(0) NOT NULL)")
         .execute(&pool)
-        .await;
+        .await
+        .expect("Wow something failed");
 
     let token = dotenv::var("TOKEN").unwrap();
     let intents = GatewayIntents::all();
