c575d9fedfca1de79fc30a0e66249740a7e2fca4
TheIneq <milromanowski@gmail.com>
2026-03-31 17:56:58 +0200

    Improved /balance command

--- src/mx/commands/balance.rs
+++ src/mx/commands/balance.rs
@@ -1,4 +1,4 @@
-use serenity::all::{CommandInteraction, Context, CreateCommand, CreateInteractionResponse, CreateInteractionResponseMessage};
+use serenity::all::{CommandInteraction, Context, CreateCommand, CreateEmbed, CreateEmbedAuthor, CreateEmbedFooter, CreateInteractionResponse, CreateInteractionResponseMessage};
 use sqlx::PgPool;
 
 use crate::mx::economy::balance;
@@ -18,8 +18,24 @@ impl Balance {
       }
 
       let balance: balance::BankAccount = balance::Balance::get_bank_account(&interaction.user.id, &pool).await;
+      let av = interaction.user.avatar_url().unwrap_or_else(|| interaction.user.default_avatar_url());
+
+      let embed = CreateEmbed::new()
+          .color((b'\xce', b'\x4d', b'\xd5'))
+          .author(
+             CreateEmbedAuthor::new(format!("Balance {}", &interaction.user.name)).icon_url(av)
+          )
+          .fields(vec![
+             ("Coins in the wallet:", format!("{} COIN", balance.wallet), false),
+             ("Coins in the bank:", format!("{} COIN", balance.bank), false),
+             ("All coins:", format!("{} COIN", balance.bank + balance.wallet), false)
+          ])
+          .footer(
+             CreateEmbedFooter::new("Version 0.1.0")
+          );
+
       let msg = interaction.create_response(&ctx.http, CreateInteractionResponse::Message(
-         CreateInteractionResponseMessage::new().content(format!("Your wallet: {}\nYour bank: {}", balance.wallet, balance.bank))
+         CreateInteractionResponseMessage::new().embed(embed)
       )).await;
 
       if let Err(e) = msg {
